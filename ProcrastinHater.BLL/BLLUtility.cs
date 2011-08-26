995a8379af45bba2ca408b33b246f2b01651b004
adnivara <acemania777@yahoo.com>
2011-08-27 00:23:13 +0530

    Added BLLUtility.cs static class.
    Implemented GetHardSettings() method in it.

---
+++ ProcrastinHater.BLL/BLLUtility.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Linq;
+
+using ProcrastinHater.POCOEntities;
+
+namespace ProcrastinHater.BLL
+{
+	/// <summary>
+	/// General purpose stuff for use by the BLL assembly.
+	/// </summary>
+	internal static class BLLUtility
+	{
+		public static HardSettings GetHardSettings(ProcrastinHaterEntities context,
+		                                           out string err)
+		{
+			err = "";
+			
+			HardSettings settings = (from s in context.HardSettingsSet
+			                         where s.Check == true
+			                         select s).SingleOrDefault();
+			
+			if (settings == null)
+				err = "The settings row does not exist in the database!";
+			
+			return settings;
+		}
+	}
+}
