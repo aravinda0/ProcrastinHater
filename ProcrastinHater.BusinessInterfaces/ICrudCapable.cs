31894a39a0ddabd90323bb023c9aecd554b11e0d
adnivara <acemania777@yahoo.com>
2011-08-26 11:51:45 +0530

    Created BusinessInterfaces project and first iface
    -Added ICrudCapable base iface and ITasksManager.
    -Also, used git rm on what I thought was the previously deleted file DatabaseInfo.cs

---
+++ ProcrastinHater.BusinessInterfaces/ICrudCapable.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace ProcrastinHater.BusinessInterfaces
+{
+	/// <summary>
+	/// Interface for CRUD operations.
+	/// </summary>
+	public interface ICrudCapable<T>
+	{
+		T GetItemById(int id);
+		bool AddItem(T newItem, out string err);
+		bool DeleteItem(int id, out string err);
+		bool UpdateItem(int id, T newItem, out string err);
+				
+	}
+}
---
+++ ProcrastinHater.BusinessInterfaces/ITasksManager.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using ProcrastinHater.POCOEntities;
+
+namespace ProcrastinHater.BusinessInterfaces
+{
+	/// <summary>
+	/// Contract for management of 'Task' objects.
+	/// </summary>
+	public interface ITasksManager : ICrudCapable<Task>
+	{
+		
+		List<Task> GetTasksForDate(DateTime date);
+	}
+}
