a733580cc1af85ddc04a7930b73577aebae60440
adnivara <acemania777@yahoo.com>
2011-08-31 21:10:48 +0530

    Added MainWindowVM

---
+++ ProcrastinHater.ViewModels/MainWindowVM.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using ElementalMvvm;
+using ProcrastinHater.BusinessInterfaces;
+using ProcrastinHater.POCOEntities;
+
+namespace ProcrastinHater.ViewModels
+{
+	/// <summary>
+	/// The main window's viewmodel.
+	/// </summary>
+	public class MainWindowVM : CloseableViewModel
+	{
+		public MainWindowVM(ITasksManager tasksManager)
+		{
+			_tasksManager = tasksManager;
+		}
+		
+		
+		#region private fields
+		
+		ITasksManager _tasksManager;
+		
+		#endregion private fields
+	}
+}
