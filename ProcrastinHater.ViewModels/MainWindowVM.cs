92921b560b9a395615f3e572fb029636df12928e
adnivara <acemania777@yahoo.com>
2011-09-11 23:43:33 +0530

    Started with MainWindowVM code.
    [ProcrastinHater.ViewModels project]
    -Wrote ctor and initial items population helper method.
    -Defined ChecklistTree property.

--- ProcrastinHater.ViewModels/MainWindowVM.cs
+++ ProcrastinHater.ViewModels/MainWindowVM.cs
@@ -1,9 +1,13 @@
 ﻿
 using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using ElementalMvvm;
 using ProcrastinHater.BusinessInterfaces;
+using ProcrastinHater.BusinessInterfaces.BLLClasses;
+using ProcrastinHater.BusinessInterfaces.CrudHelpers;
+using ProcrastinHater.ViewModels.ChecklistElements;
+using ProcrastinHater.ViewModels.Utility;
 
 namespace ProcrastinHater.ViewModels
 {
@@ -12,12 +16,67 @@ namespace ProcrastinHater.ViewModels
 	/// </summary>
 	public class MainWindowVM : CloseableViewModel
 	{
+		
+		#region ctor
 		public MainWindowVM(ITasksManager tasksManager, IGroupsManager groupsManager,
 		                   IChecklistElementOrganizer ceOrganizer)
 		{
 			_tasksManager = tasksManager;
 			_groupsManager = groupsManager;
 			_ceOrganizer = ceOrganizer;
+			
+			SetupVmTree();
+		}
+		
+	
+		
+		private void SetupVmTree()
+		{
+			GroupBLL dummyBllRootNode = new GroupBLL(0,0, new GroupInfo(), null);
+			dummyBllRootNode.Items = _ceOrganizer.GetChecklistElementTreeForDate(DateTime.Now);
+			
+			ChecklistElementVM dummyVmRootNode = CreateVmTree(dummyBllRootNode);
+			ChecklistTree = (dummyVmRootNode as GroupVM).Items;
+		}
+		
+		#endregion ctor
+		
+		
+		public ObservableCollection<ChecklistElementVM> ChecklistTree
+		{
+			get {return _checklistTree;}
+			set
+			{
+				if (_checklistTree == value)
+					return;
+				
+				_checklistTree = value;
+				this.OnPropertyChanged("ChecklistTree");
+			}
+		}		
+		
+		
+		
+		
+		
+		private ChecklistElementVM CreateVmTree(ChecklistElementBLL bllNode)
+		{
+			ChecklistElementVM ret = null;
+			
+			if (bllNode is TaskBLL)
+				ret = EntityConverter.TaskBllToTaskVm(bllNode as TaskBLL);
+			else if (bllNode is GroupBLL)
+			{
+				ret = EntityConverter.GroupBllToGroupVm(bllNode as GroupBLL);
+				GroupVM group = ret as GroupVM;
+				
+				group.Items = new ObservableCollection<ChecklistElementVM>();
+				foreach (ChecklistElementBLL ce in (bllNode as GroupBLL).Items)
+					group.Items.Add(CreateVmTree(ce));
+				
+			}
+			
+			return ret;
 		}
 		
 		
@@ -26,6 +85,9 @@ namespace ProcrastinHater.ViewModels
 		ITasksManager _tasksManager;
 		IGroupsManager _groupsManager;
 		IChecklistElementOrganizer _ceOrganizer;
+		
+		
+		ObservableCollection<ChecklistElementVM> _checklistTree;
 		
 		#endregion private fields
 	}
